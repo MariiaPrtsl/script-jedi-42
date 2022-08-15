4b4552b8f73dec69be193054a747f530b8fb5114
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-15 16:50:50 +0300

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -1,34 +1,34 @@
 //http://www.codewars.com/kata/571edd157e8954bab500032d
-var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+let v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
 function equal1(){
-  var a=v1   
-  var b=v1   
+  let a=v1   
+  let b=v1   
   return a+b;
 }
 //Please refer to the example above to complete the following functions
 function equal2(){
-  var a=v6   //set number value to a
-  var b=v3   //set number value to b
+  let a=v6   //set number value to a
+  let b=v3   //set number value to b
   return a-b;
 }
 function equal3(){
-  var a=v1   //set number value to a
-  var b=v5   //set number value to b
+  let a=v1   //set number value to a
+  let b=v5   //set number value to b
   return a*b;
 }
 function equal4(){
-  var a=v4   //set number value to a
-  var b=v5   //set number value to b
+  let a=v4   //set number value to a
+  let b=v5   //set number value to b
   return a/b;
 }
 function equal5(){
-  var a=v6   //set number value to a
-  var b=v3   //set number value to b
+  let a=v6   //set number value to a
+  let b=v3   //set number value to b
   return a%b;
 }
 
 //http://www.codewars.com/kata/571edea4b625edcb51000d8e
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+const a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
   //select some variable to combine "Dad"
   return d1+a2+d2;
