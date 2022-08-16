2f9ee40c6294a2393837ec5f2d79985ac334681e
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-16 20:14:14 +0300

    Create day-03.js

---
+++ day-03.js
@@ -0,0 +1,11 @@
+// http://www.codewars.com/kata/571f832f07363d295d001ba8
+function trueOrFalse(val){
+  return Boolean(val).toString();
+}
+
+// http://www.codewars.com/kata/57202aefe8d6c514300001fd
+function saleHotdogs(n){
+  return n*(n<5?100:n<10?95:90);
+}
+
+//http://www.codewars.com/kata/572059afc2f4612825000d8a
