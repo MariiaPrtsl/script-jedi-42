452c129a63d2ce8cbe548c8de9bde4e5d361e914
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-19 16:46:28 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,1 +1,28 @@
+//http://www.codewars.com/kata/57216d4bcdd71175d6000560
+function padIt(str, n) {
+  while (n > 0) {
+    if (n % 2) {
+      str = "*" + str;
+    } else {
+      str = str + "*";
+    }
+    n--;
+  }
+  return str;
+}
 
+//http://www.codewars.com/kata/5721a78c283129e416000999
+function pickIt(arr) {
+  let odd = [],
+    even = [];
+  for (i of arr) {
+    if (i % 2 === 0) {
+      even.push(i);
+    } else {
+      odd.push(i);
+    }
+  }
+  return [odd, even];
+}
+
+//http://www.codewars.com/kata/5721c189cdd71194c1000b9b
