2a71316d057841c898c2a4718a61e6c66e07922d
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-20 19:50:17 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,4 +1,15 @@
 //https://www.codewars.com/kata/is-this-my-tail/train/javascript
-function correctTail(bod, tail) {
-  return bod[bod.length-1] === tail
+function correctTail(body, tail) {
+  return body[body.length-1] === tail
 }
+
+//https://www.codewars.com/kata/56f6ad906b88de513f000d96
+function bonusTime(salary, bonus) {
+  if (bonus == true) {
+    return "£" + salary * 10;
+  } else {
+    return "£" + salary;
+  }
+}
+
+//https://www.codewars.com/kata/5a58d889880385c2f40000aa
