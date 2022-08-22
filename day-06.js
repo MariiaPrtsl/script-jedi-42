1cee8143e011689f5dbc1f8d89c50c2bdbca71f3
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-22 16:58:24 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,1 +1,12 @@
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+function hero(bullets, dragons) {
+  return dragons * 2 <= bullets;
+}
 
+//https://www.codewars.com/kata/59441520102eaa25260000bf
+function unusualFive() {
+  let five = 'Five?';
+  return 'Five?'.length
+}
+
+//https://www.codewars.com/kata/maximum-multiple/train/javascript
