19cd13027dd40c78a3da9539c56d79a03f09448f
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-22 16:32:21 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -13,3 +13,8 @@ function bonusTime(salary, bonus) {
 }
 
 //https://www.codewars.com/kata/5a58d889880385c2f40000aa
+function automorphic(n) {
+  return Math.pow(n, 2).toString().endsWith(n.toString())
+    ? "Automorphic"
+    : "Not!!";
+}
