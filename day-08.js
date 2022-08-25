086a13e46164ff853823c48febdb960f4a6e8796
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-08-25 17:34:22 +0300

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -17,4 +17,33 @@ function whatNumberIsIt(n) {
 }
 
 //https://www.codewars.com/kata/57238ceaef9008adc7000603
+function colorOf(r, g, b) {
+  if (r < 16) {
+    r = "0" + r.toString(16);
+  } else {
+    r = r.toString(16);
+  }
+  if (g < 16) {
+    g = "0" + g.toString(16);
+  } else {
+    g = g.toString(16);
+  }
+  if (b < 16) {
+    b = "0" + b.toString(16);
+  } else {
+    b = b.toString(16);
+  }
+  return "#" + r + g + b;
+}
+
+//http://www.codewars.com/kata/57256064856584bc47000611
+function howManySmaller(arr, n) {
+  count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (parseFloat(arr[i].toFixed(2)) < n) {
+      count++;
+    }
+  }
+  return count;
+}
 
