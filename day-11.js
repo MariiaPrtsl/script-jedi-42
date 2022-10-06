f6890527a9e03e127574b341154ea2782987a05d
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-10-06 22:59:43 +0300

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -1,1 +1,29 @@
 https://www.codewars.com/kata/playing-with-sets-equal-or-not/train/javascript
+function areEqual(s1, s2) {
+  for (const elements of s1) {
+    if (!s2.has(elements)) {
+      return false;
+    }
+  }
+  for (const elements of s2) {
+    if (!s1.has(elements)) {
+      return false;
+    }
+  }
+  return true;
+}
+function notEqual(s1, s2) {
+  for (const elements of s1) {
+    if (!s2.has(elements)) {
+      return true;
+    }
+  }
+  for (const elements of s2) {
+    if (!s1.has(elements)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
