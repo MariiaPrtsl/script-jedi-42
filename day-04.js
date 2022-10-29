cf61d2b885539fd4197fd22290951a302fd1c631
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-10-29 16:20:01 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -13,8 +13,8 @@ function padIt(str, n) {
 
 //http://www.codewars.com/kata/5721a78c283129e416000999
 function pickIt(arr) {
-  let odd = [],
-    even = [];
+  const odd = [];
+  const even = [];
   for (i of arr) {
     if (i % 2 === 0) {
       even.push(i);
@@ -27,11 +27,11 @@ function pickIt(arr) {
 
 //http://www.codewars.com/kata/5721c189cdd71194c1000b9b
 function grabDoll(dolls) {
-  let bag = [];
-  for (var i = 0; i < dolls.length; i++) {
-    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll')
+  const bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
       bag.push(dolls[i]);
-    else continue;
+    } else continue;
     if (bag.length === 3) break;
   }
   return bag;
@@ -39,11 +39,10 @@ function grabDoll(dolls) {
 
 //https://www.codewars.com/kata/5722b3f0bd5583cf44001000
 function giveMeFive(obj) {
-  let five = [];
+  const five = [];
   for (key in obj) {
     if (key.length === 5) five.push(key);
     if (obj[key].length === 5) five.push(obj[key]);
   }
   return five;
 }
-
