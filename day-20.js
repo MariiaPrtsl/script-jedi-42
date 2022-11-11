f3193f6df8b04ad82beac2563bfe7754844e0790
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-11-11 12:45:21 +0200

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -1,1 +1,7 @@
+//  https://www.codewars.com/kata/573bca07dffc1aa693000139
 
+const regex=/^-?9\d*0{4}$/
+
+//   http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+const regex=/htt(p:|ps:)\/\/[a-z\d.]+\.(com|net)/ig;
