aeee224f25ca8869ea2a0737e6f11953da26743f
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-11-11 12:53:34 +0200

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -9,6 +9,19 @@ function threeInOne(arr) {
 
 //http://www.codewars.com/kata/572df796914b5ba27c000c90
 
+function sortIt(arr) {
+  return arr.slice().sort((a, b) => {
+    const countA = arr.filter((numb) => numb === a).length;
+    const countB = arr.filter((numb) => numb === b).length;
+    if (countA === countB) {
+      return b - a;
+    } else if (countA > countB) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
 
 
 //http://www.codewars.com/kata/572fdeb4380bb703fc00002c
