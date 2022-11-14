b2f3ea84990542be41500299eace5d8f0e5b8360
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-11-14 20:44:38 +0200

    Update day-22.js

--- day-22.js
+++ day-22.js
@@ -1,1 +1,6 @@
+// https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
+
+const anyArrows = arrows => arrows.some(arrow => !arrow.damaged);
+
+//https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
 
