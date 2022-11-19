6d466c91ff81700cb43b14153fe9818e7280330a
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-11-19 11:35:25 +0200

    Create day-27.js

---
+++ day-27.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+
+function buildFun(n) {
+  const res = [];
+
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
+// https://www.codewars.com/kata/shifty-closures/train/javascript
+
+const greet_abe = function () {
+  let name = "Abe";
+  return "Hello, " + name + "!";
+};
+name = "Ben";
+const greet_ben = function () {
+  return "Hello, " + name + "!";
+};
