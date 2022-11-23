5ef466f302ac0fca835cef9684dcffd60a7545f9
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-11-23 13:28:59 +0200

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -1,1 +1,38 @@
-//
+// http://www.codewars.com/kata/santaclausable-interface
+
+function isSantaClausable(obj) {
+  if (
+    typeof obj.sayHoHoHo === "function" &&
+    typeof obj.distributeGifts === "function" &&
+    typeof obj.goDownTheChimney === "function"
+  ) {
+    return true;
+  } else return false;
+}
+
+//http://www.codewars.com/kata/cylon-evolution
+
+class Cylon {
+  constructor(model) {
+    this.model = model;
+  }
+  attack() {
+    return "Destroy all humans!";
+  }
+}
+
+class HumanSkin extends Cylon {
+  constructor(model) {
+    super(model);
+  }
+  infiltrate() {
+    return "Infiltrate the colonies";
+  }
+}
+
+// http://www.codewars.com/kata/write-javascripts-call-function-using-apply
+
+Function.prototype.call = function (/**/) {
+  const args = Array.prototype.slice.apply(arguments, [1]);
+  return this.apply(arguments[0], args);
+};
