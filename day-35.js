402879131ca8839434c9766d037f25f2ca014699
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-12-01 11:58:01 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -14,4 +14,12 @@ class Animal {
   }
 }
 
-//
+//http://www.codewars.com/kata/power-bind
+
+Function.prototype.bind = function (ctx) {
+  const func = this;
+  return function (...args) {
+    const rightCtx = this === global ? ctx : this;
+    return func.apply(rightCtx, args);
+  };
+};
