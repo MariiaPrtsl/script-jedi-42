6bda30cd20e8583e12db775f2cd43073c3c68c42
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-12-01 11:01:01 +0200

    Create day-35.js

---
+++ day-35.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
+
+Object.prototype.description = "TODO: GIVE ME A DESCRIPTION"
+
+//https://www.codewars.com/kata/javascript-class-like-objects
+
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`;
+  }
+}
+
+//
