dcb117323676c4f994af75e8da0f52683050508c
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-12-11 11:49:49 +0200

    Create day-36.js

---
+++ day-36.js
@@ -0,0 +1,27 @@
+//https://www.codewars.com/kata/587593285448632b8d000143
+
+function submitOrder(user) {
+  let shoppingCart, zipCode, shippingRate, orderSuccessful;
+
+  // Get the current user's shopping cart
+  OrderAPI.getShoppingCartAsync(user).then(function (cart) {
+    shoppingCart = cart;
+    return shoppingCart;
+  });
+
+  // Also look up the ZIP code from their profile
+  CustomerAPI.getProfileAsync(user)
+    .then(function (profile) {
+      zipCode = profile.zipCode;
+      return zipCode;
+    })
+    .then(() => (shippingRate = calculateShipping(shoppingCart, zipCode)))
+
+    // Submit the order
+    .then(() => OrderAPI.placeOrderAsync(shoppingCart, shippingRate))
+    .then((success) => (orderSuccessful = success));
+
+  console.log(
+    `Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`
+  );
+}
