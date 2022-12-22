4f3f90a485c187bf5f39f05167f0e8e576149b51
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-12-22 20:39:23 +0200

    Update day-40.js

--- day-40.js
+++ day-40.js
@@ -1,11 +1,14 @@
 //https://www.codewars.com/kata/542106e2dda52658bf00001a/train/javascript
 
 
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 async function execute(command, callback) {
   exec(command, (error, res) => {
-    if (error) { callback(error) }
-    else { callback(null, res) }
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, res);
+    }
   });
 }
