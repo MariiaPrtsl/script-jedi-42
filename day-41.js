62493bf6770c8baaae6594fac165054b681bfa87
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-12-23 23:02:49 +0200

    Create day-41.js

---
+++ day-41.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/567ed5db4089538eea000010/train/javascript
+
+function launchAll(launchMissile) {
+  for(let i = 0; i < 5; i++) {
+    setTimeout(function() {
+      launchMissile(i);
+    }, i * 1000);
+  }
+}
+
+// https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
