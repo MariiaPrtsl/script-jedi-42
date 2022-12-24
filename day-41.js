bbb3d3f544155b09fcf5315c2559b20acf33a0b2
MariiaPrtsl <108431414+MariiaPrtsl@users.noreply.github.com>
2022-12-24 12:51:38 +0200

    Update day-41.js

--- day-41.js
+++ day-41.js
@@ -9,3 +9,12 @@ function launchAll(launchMissile) {
 }
 
 // https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
+
+function queueTime(customers, n) {
+  const tills = new Array(n).fill(0);
+  for (let waitTime of customers) {
+    const lowestWaitTill = tills.indexOf(Math.min(...tills));
+    tills[lowestWaitTill] += waitTime;
+  }
+  return Math.max(...tills);
+}
